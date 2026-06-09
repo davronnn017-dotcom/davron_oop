5789c90982f87c4ec7983acda241c1d4f6e0be3b
davronnn017-dotcom <davronnn017@gmail.com>
2026-06-09 04:55:52 +0000

    My fisrt java project

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+     Student student1= new Student("Davron",20,3.00);
+    
+      student1.displayInfo();
+      
+    }
+    
+}
+
+
